a618aa63f32795cdc0b50cbdd85dd7087b3d622a
Nathan Miller <natem31190@gmail.com>
2021-01-01 17:33:56 -0500

    commit tree f6c3b71c38f3c494be73b9c1aaef39d25a867db5

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog
+    # def initialize(name)
+        #  @name = name
+         
+    # end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+         @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+end
+    
+# fido = Dog.new("Fido", "Beagle")
+# n = fido.name
+# b = fido.breed
+# puts n + " " + b
+   # dog_obj_2 = Dog.new("Pete")
+
+    # Dog.breed=("Corgi")
+# breed = Dog.new("Corgi")
